19903efa3c466752d9053c6c193251ccbc784800
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-14 13:46:43 -0700

    initi data connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+
+
 function App() {
 
   return (
+    <div className="card-container">
+      {players.map((player) => (
+        <Player
+          key = {player.name}
+          position = {player.position}
+          stats = {player.stats}
+        />
+      ))}
+    </div>
   )
 }
 

392c50927f21cd3d45fac61b44fd5e2c964f7487
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-14 13:45:17 -0700

    Header and Footer components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
 function App() {
 
   return (
+    <>
+      <Header></Header>
+
+      <Footer></Footer>
+    </>
   )
 }
 

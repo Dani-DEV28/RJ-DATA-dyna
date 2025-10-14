3aad762421a6d98471821de2e65006f4e77bc021
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-14 14:13:46 -0700

    prep

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -2,7 +2,7 @@ export default function Player(player) {
     return (
         <div className="player-card">
             <h2>{player.name}</h2>
-            {console.log(player.key)}
+            {/* {console.log(player.key)} */}
         </div>
     );
 }
